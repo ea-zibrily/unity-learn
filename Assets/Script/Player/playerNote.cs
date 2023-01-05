cfc6574457652438a5a7aae6886727e9b24b8cc9
Zibrrr <zyuuannn@gmail.com>
2023-01-05 12:08:16 +0700

    lupa push

--- Assets/Script/Player/playerNote.cs
+++ Assets/Script/Player/playerNote.cs
@@ -4,29 +4,48 @@ using UnityEngine;
 
 public class playerNote : MonoBehaviour
 {
+
+    // Path: Assets\Script\Player\playerNote.cs
+    // Compare this snippet from Assets\Script\Player\playerMove.cs
+
     /*
-    void flip()
-    {
-        isLeft = !isLeft;
-        idleDirection.x *= -1;
-        attackDirection.x *= -1;
-    }
+    
+        // Compare Code #1
+        // Code buat atur direction player waktu ngelock target
+            if (aimDir > 0 && !isLock)
+            {
+                myAnim.SetFloat("Hori", targetDirection.x);
+                myAnim.SetFloat("Vert", targetDirection.y);
+            }
+            else if (aimDir < 0 && !isLock)
+            {
+                transform.localScale = new Vector2(-1, 1);
+            }
 
-    void flipTowardPlayer()
-    {
-        float move = player.position.x - transform.position.x;
+            float aimX, aimY;
+            #region AimX
+            if (crossHair.transform.position.x <= 0)
+            {
+                aimX = crossHair.transform.position.x / (crossHair.transform.position.x * (-1));
+            }
+            else
+            {
+                aimX = crossHair.transform.position.x / crossHair.transform.position.x;
+            }
+            #endregion
 
-        //puter
-        if (move > 0 && isLeft)
-        {
-            flip();
-            transform.localScale = new Vector2(rightX, transform.localScale.y);
-        }
-        else if (move < 0 && !isLeft)
-        {
-            flip();
-            transform.localScale = new Vector2(leftX, transform.localScale.y);
-        }
-    }
+            #region AimY
+            if (crossHair.transform.position.y <= 0)
+            {
+                aimY = crossHair.transform.position.y / (crossHair.transform.position.y * (-1));
+            }
+            else
+            {
+                aimY = crossHair.transform.position.y / crossHair.transform.position.y;
+            }
+            #endregion
+
+            targetDirection = new Vector2(aimX, aimY);
+
     */
 }
